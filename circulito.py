db426f5a0968e629493969bbe04d140cde9dfe70
gpm231 <gpalacio20b@gmail.com>
2023-01-29 01:56:32 +0100

    cosas

--- circulito.py
+++ circulito.py
@@ -12,10 +12,10 @@ def crearCirculo(velocidad, divisiones):
     stepAngulo = math.pi/divisiones
     angulo = 0
 
-    while angulo < 2*math.pi:
+    while True:#angulo < 2*math.pi:
         plano.resetear()
         plano.dibujarVector("{}, {}".format(round(math.cos(angulo)*tamañoPlano*1.35), round(math.sin(angulo)*tamañoPlano)), "green") # el *1.35 por lo que pone en la línea en la que creamos el plano
         angulo += stepAngulo
         time.sleep(1/velocidad)
 
-crearCirculo(5, 30)
+crearCirculo(40, 30)
